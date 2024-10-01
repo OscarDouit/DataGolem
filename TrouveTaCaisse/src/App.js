20a1085469181fad6afbc1cfefc5dca112bd9393
Lucie_LF <105053818+LucieLeFur@users.noreply.github.com>
2024-10-01 11:11:36 +0200

    feature/loginPage

--- TrouveTaCaisse/src/App.js
+++ TrouveTaCaisse/src/App.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+  );
 }
 
 export default App;
